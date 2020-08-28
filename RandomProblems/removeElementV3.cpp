823a667266d7fcaee0c2a6515b99b90f119668ec
Dheeraj <Dheeraj.embdeveloper@gmail.com>
2020-08-28 20:49:16 +0530

    Solved Remove element V3

---
+++ RandomProblems/removeElementV3.cpp
@@ -0,0 +1,22 @@
+// solved by Techie Dheeraj
+// https://leetcode.com/problems/remove-element
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int arrIdx = 0;
+        int size = nums.size();
+        
+        for (int i = 0; i < size; ++i) {
+            if (nums[i] != val) {                
+                if (arrIdx != i) {
+                   nums[arrIdx] ^= nums[i];
+                   nums[i] ^= nums[arrIdx];
+                   nums[arrIdx] ^= nums[i];
+                }
+                arrIdx++;
+            }        
+        }
+        return arrIdx;
+    }
+};
