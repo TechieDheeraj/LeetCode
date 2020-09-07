3119cdc62969dc7d3f88c64208ddbe17bdbca33f
Techie Dheeraj <dheeraj.embdeveloper@gmail.com>
2020-09-07 11:36:17 +0530

    solved triplet Subseq problem

---
+++ RandomProblems/tripletSubseq.cpp
@@ -0,0 +1,20 @@
+// solved by Techie Dheeraj
+// https://leetcode.com/problems/increasing-triplet-subsequence/
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        
+      int c1 = INT_MAX, c2 = INT_MAX;
+      
+      for(auto x : nums) {
+        if (x <= c1)
+          c1 = x;
+        else if (x <= c2)
+          c2 = x;
+        else 
+          return true;
+      }
+      return false;
+    }
+};
