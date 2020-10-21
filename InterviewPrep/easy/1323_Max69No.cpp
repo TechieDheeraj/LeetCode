35dcdd55bbe33698b138bb79b922d609dcd39076
Techie Dheeraj <dheeraj.embdeveloper@gmail.com>
2020-10-22 00:44:18 +0530

    Solved https://leetcode.com/problems/maximum-69-number/

---
+++ InterviewPrep/easy/1323_Max69No.cpp
@@ -0,0 +1,40 @@
+/**
+ *      file    :  1323_Max69No.cpp
+ *      author  :  techiedheeraj
+ *      created :  2020 Oct 22 12:43:57 AM
+ *      lastMod :  Thu 22 Oct 2020 12:43:57 AM IST
+**/
+
+class Solution {
+public:
+    int maximum69Number (int num) {
+      
+      int n = num;
+      int tmp = 0;
+      int r = 0;
+      int finalN = 0;
+      bool isfound = false;
+      stack<int>result;
+      
+      while (n) {
+        tmp = n % 10;
+        result.push(tmp); 
+        n /= 10;
+      }
+      
+      while(result.size() > 0) {
+        int intr = result.top();
+        if (intr == 6 && !isfound) {
+          isfound = true;
+          result.pop();
+          result.push(9);
+        }
+        
+        finalN *= 10;  
+        finalN += result.top();
+        result.pop();
+      }
+      
+      return finalN;
+    }
+};
