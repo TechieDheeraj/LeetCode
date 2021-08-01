89d78320361c6205cbf3e31204ef6a62cf4c6e08
Dheeraj <dheeraj.embdeveloper@gmail.com>
2021-08-01 16:27:01 +0530

    Solved https://leetcode.com/problems/move-zeroes/

--- InterviewPrep/easy/283_moveZeros.cpp
+++ InterviewPrep/easy/283_moveZeros.cpp
@@ -23,3 +23,30 @@ class Solution {
     }
   }
 };
+
+// second solution
+//
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+      int idx = 0;
+      bool isZero = false;
+      if (nums.size() == 1) return;
+
+      for (int i = 0;i < nums.size(); ++i) {
+        if (nums[i] != 0) {
+          if (i == 0)
+            idx = 1;
+          else if (isZero) {
+            nums[idx] = nums[i];
+            nums[i] = 0;
+            idx++;
+          }
+          else
+            idx++;
+        } else
+          isZero = true;
+      }
+    }
+};
