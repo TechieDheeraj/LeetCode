c5eddebe0e41d1aa039e8d3c72ebea4064a2c260
Dheeraj <dheeraj.embdeveloper@gmail.com>
2021-08-14 15:11:14 +0530

    Solved https://leetcode.com/problems/find-the-duplicate-number/

--- InterviewPrep/medium/287_findTheDuplicateNum.cpp
+++ InterviewPrep/medium/287_findTheDuplicateNum.cpp
@@ -78,3 +78,84 @@ public:
       return -1;
     }
 };
+
+// Solutions
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+      // n^2
+
+      /*
+      for (int i =0;i < nums.size()-1; ++i) {
+        for (int j =i+1;j < nums.size(); ++j) {
+          if (nums[i] == nums[j])
+            return nums[i];
+        }
+      }
+      return -1;
+      */
+
+      // O(n) SC: O(n)
+      /*
+      unordered_set<int>hs;
+      for (int i =0; i< nums.size(); ++i) {
+        if (hs.find(nums[i]) != hs.end())
+          return nums[i];
+        hs.insert(nums[i]);
+      }
+      return -1;
+      */
+
+      // [1, 3, 4, 2, 2] -> [1, n] -> indexes [0, n-1]
+
+      // O(n)
+      /*
+      for (int i =0;i < nums.size(); ++i) {
+        if (nums[abs(nums[i])] < 0)
+          return abs(nums[i]);
+
+        nums[abs(nums[i])] *= -1;
+      }
+      return -1;
+      */
+      /*
+      int n = nums.size()-1;
+      int s = (n * (n+1)) /2;
+
+      int sum = 0;
+      for (auto &i : nums)
+        sum+=i;
+      return abs(s-sum);
+      */
+
+      // set bits of integer and if the bit is already set that means num is duplicate
+      /* only work if n <= 31
+      int seen = 0;
+      for (auto&i: nums) {
+        if (seen & (1 << i))
+          return i;
+        seen |= 1 << i;
+      }
+      return -1;
+      */
+
+
+      if (nums.size() > 1) {
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+
+        while(slow != fast) {
+          slow=nums[slow];
+          fast=nums[nums[fast]];
+        }
+        fast = 0;
+        while(slow != fast) {
+          slow = nums[slow];
+          fast = nums[fast];
+        }
+        return slow;
+      }
+      return -1;
+    }
+};
